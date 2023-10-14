3bbd2edf6f3a32be71865a18091b3227482d7892
Jarrod Ribble <jarrod.ribble@gmail.com>
2023-10-14 03:52:19 -0600

    FSA for the Queries keyword. This one is just like the other keyword FSAs, not complicated.
    The one thing to note is that the keyword must match exactly "Queries" - case matters. This is true of the other keywords as well, so those will need to change.

---
+++ project1_classes/fsa_classes/queries_fsa.py
@@ -0,0 +1,87 @@
+from .fsa import FSA
+
+class QueriesFSA(FSA):
+    def __init__(self) -> None:
+        FSA.__init__(self, "QUERIES")
+        self.accept_states.add(self.S7)
+
+    def S0(self):
+        current_input = self._FSA__get_current_input()
+        next_state: function = None
+        if current_input == 'Q' :
+            self.num_chars_consumed += 1
+            next_state = self.S1
+        else:
+            next_state = self.S_err
+        return next_state
+    
+    def S1(self):
+        current_input = self._FSA__get_current_input()
+        next_state: function = None
+        if current_input == 'u' :
+            self.num_chars_consumed += 1
+            next_state = self.S2
+        else:
+            next_state = self.S_err
+        return next_state
+    
+    def S2(self):
+        current_input = self._FSA__get_current_input()
+        next_state: function = None
+        if current_input == 'e' :
+            self.num_chars_consumed += 1
+            next_state = self.S3
+        else:
+            next_state = self.S_err
+        return next_state
+    
+    def S3(self):
+        current_input = self._FSA__get_current_input()
+        next_state: function = None
+        if current_input == 'r' :
+            self.num_chars_consumed += 1
+            next_state = self.S4
+        else:
+            next_state = self.S_err
+        return next_state
+    
+    def S4(self):
+        current_input = self._FSA__get_current_input()
+        next_state: function = None
+        if current_input == 'i' :
+            self.num_chars_consumed += 1
+            next_state = self.S5
+        else:
+            next_state = self.S_err
+        return next_state
+    
+    def S5(self):
+        current_input = self._FSA__get_current_input()
+        next_state: function = None
+        if current_input == 'e' :
+            self.num_chars_consumed += 1
+            next_state = self.S6
+        else:
+            next_state = self.S_err
+        return next_state
+    
+    def S6(self):
+        current_input = self._FSA__get_current_input()
+        next_state: function = None
+        if current_input == 's' :
+            self.num_chars_consumed += 1
+            next_state = self.S7
+        else:
+            next_state = self.S_err
+        return next_state
+    
+    def S7(self):
+        current_input: str = self._FSA__get_current_input()
+        next_state: function = self.S7 #loop in accept
+        return next_state
+    
+    def S_err(self):
+        current_input: str = self._FSA__get_current_input()
+        next_state: function = self.S_err #loop in error
+        return next_state
+
