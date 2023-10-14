0346b2e54b212823ce8d0c65d369ed14f2a6d57f
Jarrod Ribble <jarrod.ribble@gmail.com>
2023-10-14 03:58:36 -0600

    Add all of the new FSAs to the lexer. I'm processing them in alphabetical order except for the undefined FSA. If for some reason both a valid FSA and the undefined FSA accept, the valid FSA should win because the undefined FSA should be a single character and won't replace the existing "best".
    Take out the error handling that inserts the UNDEFINED token. The undefined FSA will do this now.
    
    When adding tokens to the output, ignore WHITESPACE tokens. That FSA is just there to eat up white space.
    
    If there is an UNDEFINED token we are supposed to stop outputting tokens and change the total message to state there was an error on the line.

--- project1_classes/lexer_fsm.py
+++ project1_classes/lexer_fsm.py
@@ -1,19 +1,22 @@
 from project1_classes.fsa_classes.add_fsa import AddFSA
-from project1_classes.fsa_classes.comma_fsa import CommaFSA
+from project1_classes.fsa_classes.colon_dash_fsa import ColonDashFSA
 from project1_classes.fsa_classes.colon_fsa import ColonFSA
+from project1_classes.fsa_classes.comma_fsa import CommaFSA
 from project1_classes.fsa_classes.comment_fsa import CommentFSA
 from project1_classes.fsa_classes.facts_fsa import FactsFSA
+from project1_classes.fsa_classes.id_fsa import IdFSA
 from project1_classes.fsa_classes.left_paren_fsa import LeftParenFSA
 from project1_classes.fsa_classes.multiply_fsa import MultiplyFSA
 from project1_classes.fsa_classes.period_fsa import PeriodFSA
 from project1_classes.fsa_classes.q_mark_fsa import QMarkFSA
+from project1_classes.fsa_classes.queries_fsa import QueriesFSA
 from project1_classes.fsa_classes.right_paren_fsa import RightParenFSA
 from project1_classes.fsa_classes.rules_fsa import RulesFSA
 from project1_classes.fsa_classes.schemes_fsa import SchemesFSA
 from project1_classes.fsa_classes.string_fsa import StringFSA
+from project1_classes.fsa_classes.undefined_fsa import UndefinedFSA
 from project1_classes.fsa_classes.whitespace_fsa import WhitespaceFSA
 from .fsa_classes.fsa import FSA
-from .fsa_classes.colon_dash_fsa import ColonDashFSA
 from .token import Token
 from typing import Callable as function
 
@@ -22,26 +25,46 @@ class LexerFSM:
         self.tokens: list[Token] = []
 
         #needed FSA's
+        self.add_fsa: AddFSA = AddFSA()
         self.colon_dash_fsa: ColonDashFSA = ColonDashFSA()
         self.colon_fsa: ColonFSA = ColonFSA()
+        self.comma_fsa: CommaFSA = CommaFSA()
+        self.comment_fsa: CommentFSA = CommentFSA()
+        self.facts_fsa: FactsFSA = FactsFSA()
+        self.id_fsa: IdFSA = IdFSA()
         self.left_paren_fsa: LeftParenFSA = LeftParenFSA()
-        self.right_paren_fsa: RightParenFSA = RightParenFSA()
-        self.comma_fsa: CommaFSA = CommaFSA() 
+        self.multiply_fsa: MultiplyFSA = MultiplyFSA()
         self.period_fsa: PeriodFSA = PeriodFSA()
         self.q_mark_fsa: QMarkFSA = QMarkFSA()
-        self.multiply_fsa: MultiplyFSA = MultiplyFSA()
-        self.add_fsa: AddFSA = AddFSA()
-        self.comment_fsa: CommentFSA = CommentFSA()
-        self.schemes_fsa: SchemesFSA = SchemesFSA()
-        self.facts_fsa: FactsFSA = FactsFSA()
+        self.queries_fsa: QueriesFSA = QueriesFSA()
+        self.right_paren_fsa: RightParenFSA = RightParenFSA()
         self.rules_fsa: RulesFSA = RulesFSA()
+        self.schemes_fsa: SchemesFSA = SchemesFSA()
         self.string_fsa: StringFSA = StringFSA()
+        self.undefined_fsa: UndefinedFSA = UndefinedFSA()
         self.whitespace_fsa: WhitespaceFSA = WhitespaceFSA()
 
         #FSA manager dictionary
-        self.fsas: list[function] = [self.colon_dash_fsa, self.colon_fsa, self.left_paren_fsa, self.right_paren_fsa, self.comma_fsa, self.period_fsa, 
-                                         self.q_mark_fsa, self.multiply_fsa, self.add_fsa, self.comment_fsa, 
-                                         self.schemes_fsa, self.facts_fsa, self.rules_fsa, self.string_fsa, self.whitespace_fsa]
+        self.fsas: list[function] = [
+            self.add_fsa,
+            self.colon_dash_fsa,
+            self.colon_fsa,
+            self.comma_fsa,
+            self.comment_fsa,
+            self.facts_fsa,
+            self.id_fsa,
+            self.left_paren_fsa,
+            self.multiply_fsa,
+            self.period_fsa,
+            self.q_mark_fsa,
+            self.queries_fsa,
+            self.right_paren_fsa,
+            self.rules_fsa,
+            self.schemes_fsa,
+            self.string_fsa,
+            self.whitespace_fsa,
+            self.undefined_fsa
+        ]
 
     
     def run(self, input: str) -> str:
@@ -57,29 +80,24 @@ class LexerFSM:
                         
                 token_tuples: list[str] = self.lex(string)
                 for token_tuple in token_tuples:
-                    # token: Token = Token(token_type, string, line_num)
-                    if (string == "") :
-                        continue
-
-                    if (token_tuple[0] == "UNDEFINED") :
-                        tokens.append(("UNDEFINED", string, line_num))
-                        for object in tokens :
-                            if isinstance(object, Token): 
-                                output_string = output_string + object.to_string() + "\n"
-                        print("Total Tokens = Error on line " +  str(line_num))
-                        exit()
-
-                    tokens.append(Token(token_tuple[0], token_tuple[1], line_num))
-                    # print(token)
+                    if token_tuple[0] != 'WHITESPACE':
+                        tokens.append(Token(token_tuple[0], token_tuple[1], line_num))
         
         EOF.set_line(line_num)
         tokens.append(EOF)
             
-        for object in tokens :
-            if isinstance(object, Token): 
-                output_string = output_string + object.to_string() + "\n"
+        error_line: int = -1
+        token: Token
+        for token in tokens :
+            output_string = output_string + token.to_string() + "\n"
+            if token.get_type() == 'UNDEFINED':
+                error_line = token.get_line()
+                break
 
-        output_string = output_string + "Total Tokens = " + str(len(tokens)) + "\n"
+        if error_line >= 0:
+            output_string = output_string + "\nTotal Tokens = Error on line " + str(error_line) + "\n"
+        else:
+            output_string = output_string + "Total Tokens = " + str(len(tokens)) + "\n"
         return output_string
     
     def lex(self, input_string: str) -> list[(str,str)]:
